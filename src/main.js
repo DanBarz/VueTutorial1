71e05853e7582d1ee31e85357d626df0ad80df5a
Daniel Allen <danielpeterallen@gmail.com>
2023-07-24 11:08:53 +1000

    First Commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+
+const app = Vue.createApp({
+    data(){
+        return {
+            product: "Socks",
+            description: "The most comfortable socks you will ever wear."
+        }
+    }
+})
