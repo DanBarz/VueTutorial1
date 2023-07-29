4b437ecfa44ad349989166992228cb6ddd01c978
Daniel Allen <danielpeterallen@gmail.com>
2023-07-30 09:05:25 +1000

    working save

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 
+
 const app = Vue.createApp({
     data(){
         return {
+            brand: 'DanVue Mastery Inc.',
             product: "Socks",
             description: "The most comfortable socks you will ever wear.",
-            image: './src/assets/socks_blue.jpg',
             url: 'https://www.google.com/',
             inStock: true,
+            selectedVariant: 0,
             inventory: 10,
             on_sale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
@@ -39,8 +41,9 @@ const app = Vue.createApp({
             }
         },
         // we can use ESX Shorthand for the above function and it looks like this:
-        update_product(variant_image){
-            this.image = variant_image
+        update_product(index){
+            this.selectedVariant = index
+            console.log(index)
         },
         remove_item:function () {
             this.cart -= 1
@@ -51,5 +54,15 @@ const app = Vue.createApp({
             }
         }
         
-    }   
+    },
+    
+    computed: {
+        title(){ 
+            return this.brand + ' ' + this.product 
+        },
+
+        image() {
+            return this.variants[this.selectedVariant].variant_image
+        }
+    }
 })
